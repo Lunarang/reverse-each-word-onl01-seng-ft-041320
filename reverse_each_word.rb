eb9983db55bfbbb67e1bd45def31152ea92913f6
Erin Mettille <ecmettille@gmail.com>
2020-04-17 00:52:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(phrase)
-  backwards = phrase.reverse
   backwards.split(" ")
-  sentence = []
   sentence << backwards
   sentence.reverse!
+  
+    array = words.split
+    new_sentence = []
+    array.each do |word|
+        word.reverse!
+        new_sentence << word
+    end
+    new_sentence.join(" ")
+end
 end
